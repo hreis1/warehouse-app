b2fb166870d076d3947b8892658414cf12984d9e
Paulo Henrique <hreispaulo1@gmail.com>
2023-10-17 01:43:27 -0300

    Usuário vê lista de Galpões

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -7,4 +7,28 @@ describe "User views homepage" do
 
     expect(page).to have_content("Galpões & Estoque")
   end
+
+  it "and sees a list of warehouses" do
+    Warehouse.create!(name: "Galpão 1", code: "ABC", city: "Cidade 1" , area: 10_000)
+    Warehouse.create!(name: "Galpão 2", code: "DEF", city: "Cidade 2" , area: 20_000)
+
+    visit root_path
+
+    expect(page).not_to have_content("Nenhum galpão cadastrado")
+    expect(page).to have_content("Galpão 1")
+    expect(page).to have_content("Código: ABC")
+    expect(page).to have_content("Cidade: Cidade 1")
+    expect(page).to have_content("10000 m²")
+
+    expect(page).to have_content("Galpão 2")
+    expect(page).to have_content("Código: DEF")
+    expect(page).to have_content("Cidade: Cidade 2")
+    expect(page).to have_content("20000 m²")
+  end
+
+  it "and sees a message if there are no warehouses" do
+    visit root_path
+
+    expect(page).to have_content("Nenhum galpão cadastrado")
+  end
 end
