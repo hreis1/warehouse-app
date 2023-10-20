d89f6442bdf9c60d939eaec0b62532cad97d25bb
Paulo Henrique <hreispaulo1@gmail.com>
2023-10-20 02:25:56 -0300

    Testes unitários model ProductModel

--- app/models/product_model.rb
+++ app/models/product_model.rb
@@ -1,3 +1,4 @@
 class ProductModel < ApplicationRecord
+  validates :name, :sku, :weight, :height, :width, :depth, presence: true
   belongs_to :supplier
 end
--- spec/models/product_model_spec.rb
+++ spec/models/product_model_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe ProductModel, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    context 'when all attributes are present' do
+      it 'false when name is blank' do
+        apple = Supplier.create!(corporate_name: "Apple", brand_name: "Apple", registration_number: "1234567891012", full_address: "Rua das Flores, 456", city: "Cidade 2", state: "CD", email: "contato@apple.com")
+        pm = ProductModel.new(name: "", weight: 194, height: 150, width: 75, depth: 8,sku: "IPHONE-123" ,supplier: apple)
+        expect(pm.valid?).to eq false
+      end
+
+      it 'false when weight is blank' do
+        apple = Supplier.create!(corporate_name: "Apple", brand_name: "Apple", registration_number: "1234567891012", full_address: "Rua das Flores, 456", city: "Cidade 2", state: "CD", email: "contato@apple.com")
+        pm = ProductModel.new(name: "Iphone 11", weight: "", height: 150, width: 75, depth: 8,sku: "IPHONE-123" ,supplier: apple)
+        expect(pm.valid?).to eq false
+      end
+      
+      it 'false when height is blank' do
+        apple = Supplier.create!(corporate_name: "Apple", brand_name: "Apple", registration_number: "1234567891012", full_address: "Rua das Flores, 456", city: "Cidade 2", state: "CD", email: "contato@apple.com")
+        pm = ProductModel.new(name: "Iphone 11", weight: 194, height: "", width: 75, depth: 8,sku: "IPHONE-123" ,supplier: apple)
+        expect(pm.valid?).to eq false
+      end
+   
+      it 'false when width is blank' do
+        apple = Supplier.create!(corporate_name: "Apple", brand_name: "Apple", registration_number: "1234567891012", full_address: "Rua das Flores, 456", city: "Cidade 2", state: "CD", email: "contato@apple.com")
+        pm = ProductModel.new(name: "Iphone 11", weight: 194, height: 150, width: "", depth: 8,sku: "IPHONE-123" ,supplier: apple)
+        expect(pm.valid?).to eq false
+      end
+   
+      it 'false when depth is blank' do
+        apple = Supplier.create!(corporate_name: "Apple", brand_name: "Apple", registration_number: "1234567891012", full_address: "Rua das Flores, 456", city: "Cidade 2", state: "CD", email: "contato@apple.com")
+        pm = ProductModel.new(name: "Iphone 11", weight: 194, height: 150, width: 75, depth: "",sku: "IPHONE-123" ,supplier: apple)
+        expect(pm.valid?).to eq false
+      end
+   
+      it 'false when sku is blank' do
+        apple = Supplier.create!(corporate_name: "Apple", brand_name: "Apple", registration_number: "1234567891012", full_address: "Rua das Flores, 456", city: "Cidade 2", state: "CD", email: "contato@apple.com")
+        pm = ProductModel.new(name: "Iphone 11", weight: 194, height: 150, width: 75, depth: 8,sku: "" ,supplier: apple)
+        expect(pm.valid?).to eq false
+      end
+   
+      it 'false when supplier is blank' do
+        apple = Supplier.create!(corporate_name: "Apple", brand_name: "Apple", registration_number: "1234567891012", full_address: "Rua das Flores, 456", city: "Cidade 2", state: "CD", email: "contato@apple.com")
+        pm = ProductModel.new(name: "Iphone 11", weight: 194, height: 150, width: 75, depth: 8,sku: "IPHONE-123" ,supplier: nil)
+        expect(pm.valid?).to eq false
+      end
+    end
+  end
 end
