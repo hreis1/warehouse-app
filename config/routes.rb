858a1879834a7fa6b5048b40e136cd39ef583e50
Paulo Henrique <hreispaulo1@gmail.com>
2023-10-20 04:14:52 -0300

    Adiciona e configura Gem Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "home#index"
   resources :warehouses, only: [:show, :new, :create, :edit, :update, :destroy]
   resources :suppliers, only: [:index, :show, :new, :create, :edit, :update]
